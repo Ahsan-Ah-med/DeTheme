b08b1d6fdc301373eb88b25d2d0cf9c0850760bb
Ahsan Ahmed <66218657+Ahsan-Ah-med@users.noreply.github.com>
2025-08-14 17:18:29 +0500

    Push extract Theme

---
+++ New Theme/assets/custom.js
@@ -0,0 +1,19 @@
+jQuery_T4NT(document).ready(function($) {
+
+     /**
+     *  Variant selection changed
+     *  data-variant-toggle="{{variant.id}}"
+     */
+	   // $( document ).on( "variant:changed", function( evt ) {
+	   //   // console.log( evt.currentVariant );
+	   //   // $('[data-variant-toggle]').hide(0);
+	   //   // $('[data-variant-toggle="'+evt.currentVariant.id+'"]').show(0);
+	   // });
+
+  
+// $( document ).on( "variant:changed", function( evt ) {
+//   console.log(evt.currentVariant.id)
+//   document.querySelector(`.main_product_hiddens select[data-groups-pr-sl] option[value="${evt.currentVariant.id}"]`).selected = true;
+// });
+  
+});
